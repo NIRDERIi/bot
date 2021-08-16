572256f7da931fc1a05291b15ab1c1920137a85a
gr-imm <a59942946@gmail.com>
2021-08-16 17:22:34 +0100

    added typehints + formatted code

--- bot.py
+++ bot.py
@@ -11,16 +11,26 @@ load_dotenv()
 
 
 class Bot(commands.Bot):
-
-    def __init__(self, command_prefix, help_command, description, **options):
-        super().__init__(command_prefix, help_command=help_command, description=description, **options)
+    def __init__(
+        self,
+        command_prefix: str,
+        help_command: commands.HelpCommand,
+        description: str,
+        **options
+    ) -> None:
+        super().__init__(
+            command_prefix,
+            help_command=help_command,
+            description=description,
+            **options
+        )
         self.allowed_users = [876834244167622677, 480404983372709908]
         self.session: typing.Optional[aiohttp.ClientSession] = None
 
     async def login(self, token: str, **kwargs):
 
         await super().login(token=token, **kwargs)
-        
+
         self.session = aiohttp.ClientSession()
 
     async def close(self):
@@ -29,24 +39,28 @@ class Bot(commands.Bot):
         await super().close()
 
     @property
-    def retrieve_token(self):
+    def retrieve_token(self) -> str:
 
-        token = os.getenv('TOKEN')
+        token = os.getenv("TOKEN")
         if not token:
-            raise EnvError('Fetching the TOKEN failed.')
+            raise EnvError("Fetching the TOKEN failed.")
         return token
 
     @property
-    def retrieve_dsn(self):
+    def retrieve_dsn(self) -> str:
 
-        dsn = os.getenv('DSN')
+        dsn = os.getenv("DSN")
         if not dsn:
-            raise EnvError('Fetching the DSN failed.')
+            raise EnvError("Fetching the DSN failed.")
 
     @property
-    def load_extensions(self):
+    def load_extensions(self) -> None:
 
-        files = [file[:3] for file in os.listdir('exts') if file.endswith('.py') and '__' not in file]
+        files = [
+            file[:3]
+            for file in os.listdir("exts")
+            if file.endswith(".py") and "__" not in file
+        ]
 
         for file in files:
 
