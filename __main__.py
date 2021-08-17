1ac27ca6c8a45789a052b7244be45341ab5952f5
gr-imm <a59942946@gmail.com>
2021-08-17 20:10:41 +0100

    time converter

--- __main__.py
+++ __main__.py
@@ -9,7 +9,7 @@ from discord import Intents
 from bot import Bot
 
 intents = Intents.default()
-intents.members = True
+intents.members = True # disabled precense since we don't use that and it's using more memory
 
 options = {
     "command_prefix": commands.when_mentioned_or("!"),
--- utils/converters.py
+++ utils/converters.py
@@ -18,3 +18,27 @@ class Limit(commands.Converter):
 
         # Can always add more restrictions
         return argument
+
+
+class TimeConverter(commands.Converter):
+    
+    async def convert(self, ctx: CustomContext, argument: str):
+
+        if argument.isdigit() or argument.isnumeric():
+            raise ProcessError("Time must have one time extension: (s, m, h, d, w)")
+
+        time_extensions = {
+            "s": 1,
+            "m": 60,
+            "h": 60*60,
+            "d": 60*60*24,
+            "w": 60*60*24*7,
+        }
+
+        arg_extension = argument[-1].lower()
+        arg_number = int(argument[:-1])
+        
+        if arg_extension not in time_extensions:
+            raise ProcessError('Time extension should be either: s, m, h, d, w (got extension: {})'.format(arg_extension))
+
+        return arg_number * time_extensions[arg_extension]
