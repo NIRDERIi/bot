12a67f205d1752115c78a5973b133a16ada9ba7b
gr-imm <a59942946@gmail.com>
2021-08-18 21:51:37 +0100

    removed self.pool.acquire

--- bot.py
+++ bot.py
@@ -30,7 +30,6 @@ class Bot(commands.Bot):
         self.pool = self.loop.run_until_complete(
             asyncpg.create_pool(dsn=self.retrieve_dsn, min_size=1, max_size=5)
         )
-        self.pool.acquire
         self.allowed_users = [
             876834244167622677,
             480404983372709908,
