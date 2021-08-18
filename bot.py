c1bb69c5d2fd45dc26f58073b1538d7c5ca3781f
gr-imm <88435018+gr-imm@users.noreply.github.com>
2021-08-18 14:03:55 +0100

    removed unused import

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
-from AnilistPython import Anilist
 import utils
 import typing
 import aiohttp
@@ -31,7 +30,6 @@ class Bot(commands.Bot):
         # self.pool = self.loop.run_until_complete(asyncpg.create_pool(dsn=self.retrieve_dsn, min_size=1, max_size=5))
         self.allowed_users = [876834244167622677, 480404983372709908]
         self.session: typing.Optional[aiohttp.ClientSession] = None
-        self.anilist = Anilist()
         self.uptime = datetime.datetime.utcnow()
 
     async def login(self, token: str, **kwargs) -> None:
